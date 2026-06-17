68c10b516f5793251804f6733200d40509753479
silveriomaryann21-svg <silveriomaryann21@gmail.com>
2026-06-17 15:27:37 +0800

    Keeping one message

--- codingtracker/Program.cs
+++ codingtracker/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-
+Console.WriteLine("Hello, C# Academy! One more modification!"); 
